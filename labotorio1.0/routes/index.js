351bcaa0a4d2ecc0f9b2ba39300c90058c636971
David H <davyd2h@gmail.com>
2026-05-21 06:28:47 -0500

    se crea conexiones a base de datos local utilizando el dialeto sqlite

--- labotorio1.0/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { getUser } from "./user.routes.js";
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/user', getUser);
-
-export default router;
--- labotorio1.0/routes/user.routes.js
+++
@@ -1,3 +0,0 @@
-export const getUser = (req, res, next) => {
-    res.send('mi ruta de inicio funcionando ok')
-}
