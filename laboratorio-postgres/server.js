cbcdcbc38ecf68e6593e85b7cbbe00074f528081
David Hdez <davyd2h@gmail.com>
2026-05-22 14:45:39 -0500

    se corrige la conexión a base de datos, tal como lo pide prisma 7, y se organiza en un archivo aparte

--- laboratorio-postgres/server.js
+++ laboratorio-postgres/server.js
@@ -3,6 +3,7 @@ import e from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
+import { prisma } from './config/prisma.js'
 
 const __dirfile = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__dirfile);
@@ -12,8 +13,11 @@ const app = e();
 app.use(e.json({ limit: '2mb' }));
 app.use(e.static(rutaAbsoluta)); //Servimos nuestro frontend
 
-
+async function data() {
+    const data = await prisma.user.findMany();
+    console.log(data)
+}
+data()
 
 app.listen(env.PORT, () => {
-    console.log(`Server Running in http://localhost:${env.PORT}`)
 })
